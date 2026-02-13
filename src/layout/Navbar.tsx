30a4f8b3276ffa197d2ff0865c983f5421f8b2e0
augustinyu <augustin_yu@hotmail.com>
2026-02-14 10:45:27 +1100

    Revert "remove tailwind"
    This reverts commit b45e9a942c930a5247b95c5fec74005445a07c2b.

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -30,11 +30,6 @@ export const Navbar = () => {
             ))}
           </div>
         </div>
-
-        {/* CTA Button */}
-        <div>
-          <Button></Button>
-        </div>
       </nav>
     </header>
   );
